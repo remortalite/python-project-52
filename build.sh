a2f99d642880b1cdd9bddf58120034ccf9237170
Roman Malanov <morrig407@gmail.com>
2024-08-07 22:01:23 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Exit on error
-#set -o errexit
+set -o errexit
 
 make install
 
-#poetry run python manage.py collectstatic --no-input
+poetry run python manage.py collectstatic --no-input
 
-#poetry run python manage.py migrate
+poetry run python manage.py migrate
