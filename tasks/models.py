ff916c883dc4375bc65ca0b8d9978222005c4833
Rem M. <morrig407@gmail.com>
2024-08-12 19:54:43 +0700

    Add model Task

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from statuses.models import Status
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+
+    assigned_to = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True)
+    status = models.ForeignKey(to=Status, on_delete=models.CASCADE)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
