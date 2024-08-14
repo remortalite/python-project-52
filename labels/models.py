2acea10ff9a45a4a6742d08c6d61aebb1c9b3280
Rem M. <morrig407@gmail.com>
2024-08-14 21:50:27 +0700

    Add model Label

--- labels/models.py
+++ labels/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Label(models.Model):
+    name = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.name
