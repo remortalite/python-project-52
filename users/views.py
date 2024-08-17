11fc173374cf832b04a31e79b0b2a220748846f5
Rem M. <morrig407@gmail.com>
2024-08-17 19:09:07 +0700

    Replace objectget method with get_object_or_404

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django import views
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
 from django.utils.translation import gettext as _
 from django.contrib.auth.models import User
@@ -40,18 +40,17 @@ class UserUpdateView(LoginRequiredMixin, views.View):
             messages.error(request, _("У вас нет прав для изменения "
                                       "другого пользователя."))
             return redirect(reverse('users'))
-        user = User.objects.get(id=id)
+        user = get_object_or_404(User, id=id)
         form = UserUpdateForm(instance=user)
-        if user:
-            return render(request, "users/update.html",
-                          {"form": form, "user_id": id})
+        return render(request, "users/update.html",
+                      {"form": form, "user_id": id})
 
     def post(self, request, id, *args, **kwargs):
         if request.user.id != id:
             messages.error(request, _("У вас нет прав для изменения "
                                       "другого пользователя."))
             return redirect(reverse('users'))
-        user = User.objects.get(id=id)
+        user = get_object_or_404(User, id=id)
         form = UserUpdateForm(data=request.POST, instance=user)
         if form.is_valid():
             form.save()
@@ -74,7 +73,7 @@ class UserDeleteView(LoginRequiredMixin, views.View):
             messages.error(request, _("У вас нет прав для изменения "
                                       "другого пользователя."))
             return redirect(reverse('users'))
-        user = User.objects.get(id=id)
+        user = get_object_or_404(User, id=id)
         return render(request, "users/delete.html",
                       {"user": user})
 
@@ -84,7 +83,7 @@ class UserDeleteView(LoginRequiredMixin, views.View):
                                       "другого пользователя."))
             return redirect(reverse('users'))
         try:
-            user = User.objects.get(id=id)
+            user = get_object_or_404(User, id=id)
             user.delete()
             messages.info(request, _("Пользователь успешно удален"))
         except ProtectedError:
