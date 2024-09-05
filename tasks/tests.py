b924fdcad14b91d44084053cff888cd4817a57e1
Rem M. <morrig407@gmail.com>
2024-09-05 21:14:03 +0700

    Tests: add fixtures for Task

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,55 +1,25 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
-import logging
 
 from users.models import User
 from statuses.models import Status
 from tasks.models import Task
 
-logger = logging.getLogger(__name__)
-
-USER = {"username": "test_user"}
-LOGIN_DATA = {
-    "username": USER["username"],
-    "password": USER["username"]
-}
-STATUS = {"name": "test_status"}
-
 
 class TasksTest(TestCase):
+    fixtures = ["sample.json"]
 
     def setUp(self):
-        if not User.objects.filter(**USER).exists():
-            User.objects.create_user(**LOGIN_DATA)
-
-        self.client = Client()
-        self.client.login(**LOGIN_DATA)
-
-        self.status = Status.objects.create(**STATUS)
-        self.user = User.objects.get(**USER)
-        self.task = Task.objects.create(
-            name="test_task",
-            status=self.status,
-            author=self.user,
-        )
-
-    def tearDown(self):
-        if self.task:
-            self.task.delete()
-
-        user = User.objects.filter(**USER)
-        if user.exists():
-            user.delete()
-
-        status = Status.objects.filter(**STATUS)
-        if status.exists():
-            status.delete()
+        self.client.login(username="test_user",
+                          password="test_password")
+        self.status = Status.objects.get(name="test_status")
+        self.user = User.objects.get(username="test_user")
+        self.task = Task.objects.get(name="test_task")
 
     def test_unauthorized(self):
         self.client.logout()
 
         response = self.client.get(reverse("tasks"))
-
         self.assertEqual(response.status_code, 302)
 
     def test_TasksView(self):
@@ -72,14 +42,10 @@ class TasksTest(TestCase):
         new_task = Task.objects.filter(name="test_task_create")
         self.assertTrue(new_task.exists())
 
-        response = self.client.get(
-            reverse("tasks_create"),
-            headers={"accept-language": "en"},
-        )
+        response = self.client.get(reverse("tasks_create"),
+                                   headers={"accept-language": "en"})
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Create task", response.content)
-
-        Task.objects.get(name="test_task_create").delete()
 
     def test_TaskUpdateView(self):
         response = self.client.get(
@@ -87,7 +53,6 @@ class TasksTest(TestCase):
                     kwargs={"pk": self.task.id}),
             headers={"accept-language": "en"},
         )
-
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"test_task", response.content)
 
@@ -100,13 +65,9 @@ class TasksTest(TestCase):
                 "status": self.status.id,
             }
         )
-
         self.assertEqual(response.status_code, 302)
-
         self.assertFalse(Task.objects.filter(name="test_task").exists())
         self.assertTrue(Task.objects.filter(name="test_task_updated").exists())
-
-        Task.objects.filter(name="test_task_updated").delete()
 
     def test_TaskDeleteView(self):
         response = self.client.post(
