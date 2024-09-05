8b692069d97cc6d3368687123412fb0cc47c8c08
Rem M. <morrig407@gmail.com>
2024-09-05 20:41:34 +0700

    Tests: add fixtures to statuses

--- statuses/tests.py
+++ statuses/tests.py
@@ -1,89 +1,54 @@
-import unittest
-
-from django.test import Client
+from django.test import TestCase
 from django.urls import reverse
 import logging
 
-from users.models import User
 from statuses.models import Status
 
 logger = logging.getLogger(__name__)
 
 
-class StatusViewTest(unittest.TestCase):
+class StatusViewTest(TestCase):
+    fixtures = ["sample.json"]
 
     def setUp(self):
-        if not User.objects.filter(username="test_user").exists():
-            User.objects.create_user(username="test_user",
-                                     password="test_password")
-
-        self.client = Client()
         self.client.login(username="test_user",
                           password="test_password")
-        Status.objects.create(name="test_status")
-
-    def tearDown(self):
-        if User.objects.filter(username="test_user").exists():
-            User.objects.get(username="test_user").delete()
-        if Status.objects.filter(name="test_status").exists():
-            Status.objects.get(name="test_status").delete()
+        self.status = Status.objects.get(name="test_status")
 
     def test_read_unauthorized(self):
-
         self.client.logout()
-
         response = self.client.get(reverse("statuses"))
-
         self.assertEqual(response.status_code, 302)
-        # self.assertIn(b"alert-danger", response.content)
+
+        response = self.client.get(reverse("statuses"), follow=True)
+        self.assertEqual(response.status_code, 200)
 
     def test_read_authorized(self):
-
         response = self.client.get(reverse("statuses"))
-
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"test_status", response.content)
 
     def test_create(self):
-
         self.client.post(reverse("statuses_create"),
                          data={"name": "new_test_status"})
-
-        response = self.client.get(reverse("statuses"))
-
-        self.assertEqual(response.status_code, 200)
+        self.client.get(reverse("statuses"))
         self.assertTrue(Status.objects.filter(name="new_test_status").exists())
 
-        Status.objects.filter(name="new_test_status").delete()
-
     def test_update(self):
-
-        status = Status.objects.get(name="test_status")
         response = self.client.get(reverse("statuses_update",
-                                           kwargs={"pk": status.id}))
-
+                                           kwargs={"pk": self.status.id}))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"test_status", response.content)
 
-        response = self.client.post(reverse("statuses_update",
-                                            kwargs={"pk": status.id}),
-                                    data={"name": "test_status_updated"})
+        self.client.post(reverse("statuses_update",
+                                 kwargs={"pk": self.status.id}),
+                         data={"name": "test_status_updated"})
 
         self.assertTrue(Status.objects.filter(name="test_status_updated")
                         .exists())
         self.assertFalse(Status.objects.filter(name="test_status").exists())
 
-        Status.objects.filter(name="test_status_updated").delete()
-
     def test_delete(self):
-
-        status = Status.objects.get(name="test_status")
-
         self.client.post(reverse("statuses_delete",
-                                 kwargs={"pk": status.id}))
-
+                                 kwargs={"pk": self.status.id}))
         self.assertFalse(Status.objects.filter(name="test_status").exists())
-
-
-if __name__ == '__main__':
-    unittest.main()
