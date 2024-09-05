66974186118bb878c8bf7d5cc57144263bc86d76
Rem M. <morrig407@gmail.com>
2024-09-05 16:37:30 +0700

    Add username as str-field if full name is empty

--- users/models.py
+++ users/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     def __str__(self):
-        return self.get_full_name()
+        return self.get_full_name() or self.username
