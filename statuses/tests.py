ea2518160ae165fd37381fe59da233d79a5122eb
Rem M. <morrig407@gmail.com>
2024-09-08 21:13:19 +0700

    Edit tests for Status

--- statuses/tests.py
+++ statuses/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 import logging
 
@@ -11,53 +11,81 @@ class StatusViewTest(TestCase):
     fixtures = ["sample.json"]
 
     def setUp(self):
+        self.client = Client(headers={"Accept-Language": "en"})
         self.client.login(username="test_user",
                           password="test_password")
         self.status = Status.objects.get(name="test_status")
 
-    def test_read_unauthorized(self):
+    def test_unauthorized(self):
         self.client.logout()
-        response = self.client.get(reverse("statuses"))
-        self.assertEqual(response.status_code, 302)
 
-        response = self.client.get(reverse("statuses"), follow=True)
-        self.assertEqual(response.status_code, 200)
+        urls = [
+            reverse("statuses"),
+            reverse("statuses_create"),
+            reverse("statuses_update", kwargs={"pk": self.status.id}),
+            reverse("statuses_delete", kwargs={"pk": self.status.id}),
+        ]
 
-    def test_read_authorized(self):
+        for url in urls:
+            response = self.client.get(url, follow=True)
+            self.assertIn(b"First you need to log in", response.content)
+            self.assertEqual(reverse("login"), response.request['PATH_INFO'])
+
+    def test_StatusesView(self):
         response = self.client.get(reverse("statuses"))
+
         self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Statuses", response.content)
         self.assertIn(b"test_status", response.content)
 
-    def test_create(self):
-        self.client.post(reverse("statuses_create"),
-                         data={"name": "new_test_status"})
-        self.client.get(reverse("statuses"))
+    def test_StatusCreateView(self):
+        # POST
+        response = self.client.post(reverse("statuses_create"),
+                                    data={"name": "new_test_status"},
+                                    follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Status created", response.content)
+        self.assertEqual(reverse("statuses"), response.request['PATH_INFO'])
         self.assertTrue(Status.objects.filter(name="new_test_status").exists())
 
-    def test_update(self):
-        response = self.client.get(reverse("statuses_update",
-                                           kwargs={"pk": self.status.id}))
+        # GET
+        response = self.client.get(reverse("statuses"), follow=True)
         self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Create status", response.content)
+
+    def test_StatusUpdateView(self):
+        path_to_status_update = reverse("statuses_update",
+                                        kwargs={"pk": self.status.id})
+        # GET
+        response = self.client.get(path_to_status_update, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Edit status", response.content)
         self.assertIn(b"test_status", response.content)
 
-        self.client.post(reverse("statuses_update",
-                                 kwargs={"pk": self.status.id}),
-                         data={"name": "test_status_updated"})
+        # POST
+        response = self.client.post(path_to_status_update,
+                                    data={"name": "test_updated"},
+                                    follow=True)
 
-        self.assertTrue(Status.objects.filter(name="test_status_updated")
-                        .exists())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Status updated", response.content)
+        self.assertEqual(reverse("statuses"), response.request['PATH_INFO'])
+        self.assertTrue(Status.objects.filter(name="test_updated").exists())
         self.assertFalse(Status.objects.filter(name="test_status").exists())
 
     def test_delete(self):
+        path_to_object = reverse("statuses_delete",
+                                 kwargs={"pk": self.status.id})
         # if task exists
-        response = self.client.post(reverse("statuses_delete",
-                                    kwargs={"pk": self.status.id}))
-        self.assertEqual(response.status_code, 302)
+        response = self.client.post(path_to_object, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Deletion error", response.content)
         self.assertTrue(Status.objects.filter(name="test_status").exists())
 
-        # remove task
+        # remove tasks
         self.status.task_set.all().delete()
 
-        self.client.post(reverse("statuses_delete",
-                                 kwargs={"pk": self.status.id}))
+        response = self.client.post(path_to_object, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Status deleted", response.content)
         self.assertFalse(Status.objects.filter(name="test_status").exists())
--- statuses/views.py
+++ statuses/views.py
@@ -2,26 +2,28 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib import messages
 
 from statuses.models import Status
+from users.mixins import LoginRequiredWithMessageMixin
 
 
-class StatusListView(LoginRequiredMixin, ListView):
+class StatusListView(LoginRequiredWithMessageMixin, ListView):
     model = Status
 
 
-class StatusCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+class StatusCreateView(LoginRequiredWithMessageMixin,
+                       SuccessMessageMixin, CreateView):
     model = Status
     fields = ["name"]
     success_url = reverse_lazy("statuses")
     success_message = _("Status created")
 
 
-class StatusUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+class StatusUpdateView(LoginRequiredWithMessageMixin,
+                       SuccessMessageMixin, UpdateView):
     model = Status
     fields = ["name"]
     template_name_suffix = "_update_form"
@@ -29,7 +31,8 @@ class StatusUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     success_message = _("Status updated")
 
 
-class StatusDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+class StatusDeleteView(LoginRequiredWithMessageMixin,
+                       SuccessMessageMixin, DeleteView):
     model = Status
     success_url = reverse_lazy("statuses")
     success_message = _("Status deleted")
@@ -37,8 +40,7 @@ class StatusDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     def post(self, request, pk, *args, **kwargs):
         status = get_object_or_404(Status, id=pk)
         if status.task_set.exists():
-            messages.error(request, _("You can't delete status "
-                                      "while it's in use"))
+            messages.error(request, _("Deletion error"))
             return redirect(reverse_lazy("statuses"))
         data = super().post(request, pk, *args, **kwargs)
         return data
