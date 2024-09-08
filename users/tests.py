51a5350e7fbcb36be0bbb69204f15b7ad27d16c1
Rem M. <morrig407@gmail.com>
2024-09-08 13:05:20 +0700

    Add fixture to Users

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,4 @@
-import unittest
-
-from django.test import Client
+from django.test import TestCase
 from django.urls import reverse
 import logging
 
@@ -9,93 +7,67 @@ from users.models import User
 logger = logging.getLogger(__name__)
 
 
-class UsersViewTest(unittest.TestCase):
-    # Test: task_manager.views.UsersView
-
-    def setUp(self):
-        User.objects.create_user(first_name="test_username",
-                                 username="test_user",
-                                 password="test_password")
-
-    def tearDown(self):
-        User.objects.get(username="test_user").delete()
+class UsersViewTest(TestCase):
+    fixtures = ["sample.json"]
 
     def test_status_and_column_name(self):
-        client = Client()
-        response = client.get(reverse('users'))
+        response = self.client.get(reverse('users'))
         self.assertEqual(200, response.status_code)
-        self.assertIn(b"test_username", response.content)
+        self.assertIn(b"test_user", response.content)
 
 
-class TestUserFormView(unittest.TestCase):
+class TestUserFormView(TestCase):
+    fixtures = ["sample.json"]
 
     def setUp(self):
-        User.objects.create_user(first_name="test_first_name",
-                                 username="test_user",
-                                 password="test_password")
-
-    def tearDown(self):
-        User.objects.get(username="test_user").delete()
+        self.user = User.objects.get(username="test_user")
 
     def test_post(self):
-        client = Client()
-        client.post(reverse("users_create"),
-                    data={"username": "test_user_post",
-                          "password1": "test_user_post",
-                          "password2": "test_user_post"}
-                    )
+        self.client.post(reverse("users_create"),
+                         data={"username": "test_user_post",
+                               "password1": "test_user_post",
+                               "password2": "test_user_post"}
+                         )
         user = User.objects.get(username="test_user_post")
         self.assertEqual("test_user_post", user.username)
 
-        User.objects.get(username="test_user_post").delete()
-
     def test_update_unauthorized(self):
-        client = Client()
-
-        user = User.objects.get(username="test_user")
-
-        response = client.get(reverse("users_update",
-                                      kwargs={"pk": user.id}))
-
+        response = self.client.get(reverse("users_update",
+                                           kwargs={"pk": self.user.id}))
         self.assertEqual(response.status_code, 302)
 
     def test_update_authorized(self):
-        client = Client()
-
-        client.login(username="test_user",
-                     password="test_password")
-        user = User.objects.get(username="test_user")
-        response = client.get(reverse("users_update",
-                                      kwargs={"pk": user.id}))
+        self.client.login(username="test_user",
+                          password="test_password")
+        response = self.client.get(reverse("users_update",
+                                           kwargs={"pk": self.user.id}))
 
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"test_user", response.content)
-        self.assertIn(b"test_first_name", response.content)
 
-        client.post(reverse("logout"))
+        self.client.post(reverse("logout"))
 
-        response = client.get(reverse("users_update",
-                                      kwargs={"pk": user.id}))
+        response = self.client.get(reverse("users_update",
+                                           kwargs={"pk": self.user.id}))
         self.assertEqual(response.status_code, 302)
 
     def test_delete(self):
-        client = Client()
+        self.client.login(username="test_user",
+                          password="test_password")
+        response = self.client.post(reverse("users_delete",
+                                    kwargs={"pk": self.user.id}))
+        self.assertEqual(response.status_code, 302)
 
-        User.objects.create_user(username="test_user_delete",
-                                 password="test_delete")
-        client.login(username="test_user_delete",
-                     password="test_delete")
-        user = User.objects.get(username="test_user_delete")
+        self.client.logout()
 
-        client.post(reverse("users_delete",
-                            kwargs={"pk": user.id}))
+        # create new unbounded user
+        user = User.objects.create_user(username="test_user_delete",
+                                        password="test_password_delete")
+        self.client.login(username="test_user_delete",
+                          password="test_password_delete")
 
-        self.assertFalse(
-            User
-            .objects
-            .filter(username="test_user_delete").exists()
-        )
-
-
-if __name__ == '__main__':
-    unittest.main()
+        response = self.client.post(reverse("users_delete",
+                                            kwargs={"pk": user.id}))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(User.objects
+                         .filter(username="test_user_delete").exists())
